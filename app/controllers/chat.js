edf80dbf68174d99a40c59b0248d9160f99c48d5
Glauber Goncalves <glauber.goncalves@nimaltecnologia.corp>
2017-09-26 17:04:38 -0300

    Validação do formulario

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,22 @@
 module.exports.iniciaChat = function(application, req , res){
+  var dadosForm = req.body;
+
+  /* >>>>>>> Validação do formulario */
+
+  /* Verifica se apelido está vazio */
+  req.assert('apelido', 'Apelido é obrigatório').notEmpty();
+  /* Verifica se apelido tem de 3 a 15 caracteres */
+  req.assert('apelido', 'Apelido é deve conter entre 3 a 15 caracteres').len(3,15);
+  /* popula a variavel erros com os erros validados */
+  var erros = req.validationErrors();
+  /* se houver erros, redireciona fluxo para pagina inicial */
+  if(erros){
+    /* envia erros identificados para a pagina inicial */
+    res.render('index', {validacao : erros});
+    return;
+  }
+
+  /* <<<<<<<< Validação do formulario */
+
   res.render("chat");
 }
